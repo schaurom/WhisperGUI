de01bcda8117b6af42886117cc8e5648f2e61cff
schaurom <118014904+schaurom@users.noreply.github.com>
2023-11-10 23:02:55 +0100

    Genauigkeit auf Wortebene in Farben im Word

--- whisperGUI/GUI_Color.py
+++ whisperGUI/GUI_Color.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import whisper
 from docx import Document
-from docx.shared import Pt
+from docx.shared import Pt, RGBColor
 import time
 
 def run_whisper(audio_file_path, output_file_path):
@@ -20,31 +20,48 @@ def run_whisper(audio_file_path, output_file_path):
     #Inhalt in Variable schreiben
     result_text = result["text"]
     print("\n"+result_text)
-    word_details = result["words"]
+    result_details = result['segments']
 
     # Word
     dateiname = output_file_path
 
     # erstelle ein neues Word Objekt
     dokument = Document()
-
-    # Fügen Sie den Text in das Dokument ein und spezifizieren Sie die Schriftgröße
     absatz = dokument.add_paragraph()
-    #lauf = absatz.add_run(result_text)
-    #lauf.font.size = Pt(14)  # Ändern Sie die Schriftgröße auf 14 Pt
 
-    for word_detail in word_details:
-        word_text = word_detail["word"]
-        word_accuracy = word_detail["accuracy"]
+    # Farben definieren
+    GREEN = RGBColor(0, 128, 0)
+    ORANGE = RGBColor(255, 165, 0)
+    RED = RGBColor(255, 0, 0)
 
-        # Fügen Sie den Text in das Dokument ein und spezifizieren Sie die Schriftgröße und Farbe basierend auf der Genauigkeit
-        absatz.add_run(word_text).font.size = Pt(14)
-        if word_accuracy > 0.8:
-            absatz.runs[-1].font.color.rgb = (0, 128, 0)  # Dunkelgrün für hohe Genauigkeit
-        elif word_accuracy > 0.6:
-            absatz.runs[-1].font.color.rgb = (255, 165, 0)  # Orange für moderate Genauigkeit
-        else:
-            absatz.runs[-1].font.color.rgb = (255, 0, 0)  # Rot für niedrige Genauigkeit
+    # Klossar einfügen
+    absatz.add_run('Dunkelgrün für hohe Genauigkeit - >80%').font.size = Pt(14)
+    absatz.runs[-1].font.color.rgb = GREEN
+
+    absatz.add_run('\nOrange für moderate Genauigkeit - 60% >= 80%').font.size = Pt(14)
+    absatz.runs[-1].font.color.rgb = ORANGE
+
+    absatz.add_run('\nRot für niedrige Genauigkeit - <60%').font.size = Pt(14)
+    absatz.runs[-1].font.color.rgb = RED
+    absatz.add_run('\n--------------------------------------------------------------------------------------\n\n\n').font.size = Pt(14)
+
+
+    # alle Wörter durchiterieren
+    for element in result_details:
+        for listitem in element['words']:
+            word_text = listitem['word']
+            word_probability = listitem['probability']
+            print(word_text)
+            print(word_probability)
+
+            # Wort in das Dokument einfügen, Schriftgröße spezifizieren und Farbe basierend auf der Genauigkeit
+            absatz.add_run(word_text).font.size = Pt(14)
+            if word_probability > 0.8:
+                absatz.runs[-1].font.color.rgb = GREEN  # Dunkelgrün für hohe Genauigkeit
+            elif word_probability > 0.6:
+                absatz.runs[-1].font.color.rgb = ORANGE  # Orange für moderate Genauigkeit
+            else:
+                absatz.runs[-1].font.color.rgb = RED  # Rot für niedrige Genauigkeit
 
     
     # Speichern Sie das Dokument in einer Datei
